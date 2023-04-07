40a60993084272dde4cf378dd97642ada7546340
KQT3 <70701718+KQT3@users.noreply.github.com>
2023-04-07 16:49:36 +0200

    commit

--- internal/services/AwsS3Service.go
+++ internal/services/AwsS3Service.go
@@ -18,18 +18,18 @@ const S3Bucket = "qt3test"
 func listObjects(awsCredentials AwsCredentials) *s3.ListObjectsOutput {
 	cred := credentials.NewStaticCredentials(awsCredentials.AccessKey, awsCredentials.SecretKey, "")
 
-	sess := session.Must(session.NewSession(&aws.Config{
+	newSession := session.Must(session.NewSession(&aws.Config{
 		Credentials: cred,
 		Region:      aws.String("eu-north-1"),
 	}))
 
-	svc := s3.New(sess)
+	s3Service := s3.New(newSession)
 
-	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(S3Bucket)})
+	response, err := s3Service.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(S3Bucket)})
 
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
 
-	return resp
+	return response
 }
